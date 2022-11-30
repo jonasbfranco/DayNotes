08a81c8fa8c40b58c0f1eb1f40274a39330373b4
jonasbfranco <jonasbfranco@hotmail.com>
2022-11-29 23:01:10 -0300

    :tada: 2 fase do front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,92 @@
-import React from "react";
-import './global.css'
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
+
+import './app.css';
+import "./global.css";
+import "./sidebar.css";
+import "./main.css";
+
+import Notes from './components/notes';
 
 function App() {
+  
+  const [ title, setTitle ] = useState('')
+  const [ notes, setNotes ] = useState('')
+  const [ allNotes, setAllNotes ] = useState([])
+
+  useEffect(() => {
+    async function getAllNotes() {
+      const response = await api.get('/annotations',);
+
+      setAllNotes(response.data);
+
+    }
+
+    getAllNotes();
+
+  }, []);
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post('/annotations', {
+      title,
+      notes,
+      priority: false
+    })
+
+    setTitle('');
+    setNotes('');
+
+    setAllNotes([ ...allNotes, response.data ]);
+
+  }
+
+  useEffect(() => {
+    function enableSubmitButton() {
+      let btn = document.getElementById('btn_submit');
+      btn.style.background = '#ffd3ca';
+      if (title && notes) {
+        btn.style.background = '#eb8f7a';
+      }
+    }
+    enableSubmitButton();
+  },[title, notes]);
+
   return (
     <div id="app">
       <aside>
         <strong>Caderno de Notas</strong>
-        <form>
-
+        <form onSubmit={handleSubmit}>
           <div className="input-block">
             <label htmlFor="title">Título da Anotação</label>
-            <input />
+            <input
+              required 
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+            />
           </div>
 
           <div className="input-block">
             <label htmlFor="nota">Anotações</label>
-            <textarea></textarea>
+            <textarea
+              required
+              value={notes}
+              onChange={e => setNotes(e.target.value)}
+            />
           </div>
 
-          <button type="submit">Salvar</button>
-
+          <button id="btn_submit" type="submit">Salvar</button>
         </form>
       </aside>
       <main>
-
+        <ul>
+          {allNotes.map(data => (
+            <Notes data={data}/>
+          ))}
+        </ul>
       </main>
     </div>
   );
