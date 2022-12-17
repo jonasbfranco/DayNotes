2d591e58a07ede537957f042eed6f5fa3bed027d
jonasbfranco <jonasbfranco@hotmail.com>
2022-12-17 11:01:28 -0300

    :tada: ultimo commit

--- backend/instrucoes.md
+++ backend/instrucoes.md
@@ -1,5 +1,13 @@
 #### Instaçôes
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 * npm install mongoose
 * npm install express
 * npm install nodemon
 * npm install cors
+<<<<<<< Updated upstream
+=======
+* npm install dotenv --save
+>>>>>>> Stashed changes
--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,6 +1,13 @@
+require('dotenv').config()
+
 const mongoose = require('mongoose');
 
+<<<<<<< Updated upstream
 const dbConfig = 'mongodb+srv://root:password@cluster0.ae2xvvz.mongodb.net/annotations?retryWrites=true&w=majority';
+=======
+const dbConfig = `mongodb+srv://root:${process.env.SENHA_MONGO}@cluster0.ae2xvvz.mongodb.net/${process.env.BANCO_MONGO}?retryWrites=true&w=majority`;
+//const dbConfig = 'mongodb+srv://root:Kemily@cluster0.ae2xvvz.mongodb.net/annotations?retryWrites=true&w=majority';
+>>>>>>> Stashed changes
 
 const connection = mongoose.connect(dbConfig, {
     //useNewUrlParser: true,
